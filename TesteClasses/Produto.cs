d980c2af85676f418112d1abb06b626c444867bd
Nailson Ferreira <nailson_ferreira@hotmail.com>
2019-03-13 19:27:13 -0300

    Apenas testando classes

---
+++ TesteClasses/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace TesteClasses
+{
+    class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        private int Quantidade;
+
+        //PROPERTIE é equivalente ao get/set
+        public string Nome
+        {
+            get { return _nome; }
+            set {
+                _nome = value;
+            }
+        }
+
+        
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $"
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantidade em estoque:"
+                + Quantidade;
+        }
+
+        public void CadastrarProduto()
+        {
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome do Produto: ");
+            _nome = Console.ReadLine();
+            Console.Write("\nPreço do produto: ");
+            Preco = double.Parse(Console.ReadLine());
+            Console.Write("\nQuantidade em estoque: ");
+            Quantidade = int.Parse(Console.ReadLine());
+
+        }
+
+
+        public void AdicionarEstoque()
+        {
+            Console.Write("\nAdicione a quantidade ao estoque de {0}: ", _nome);
+            Quantidade += int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ TesteClasses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TesteClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+            /*
+            Console.WriteLine("Entre com os valores A,B e C do triagulo X: ");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com os valores A,B e C do triagulo Y: ");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+            double areaX = x.area();
+            double areaY = y.area();
+
+            Console.WriteLine("Lados do triangulo X A:{0} B:{1} C:{2} é igual a area {3}", x.A, x.B, x.C, x.area());
+            Console.WriteLine("Lados do triangulo Y A:{0} B:{1} C:{2} é igual a area {3}", y.A, y.B, y.C, y.area());
+            Console.ReadLine();
+            */
+
+            Produto pro = new Produto();
+
+            pro.CadastrarProduto();
+            Console.WriteLine(pro);
+            Console.ReadLine();
+
+            pro.AdicionarEstoque();
+            Console.WriteLine(pro);
+            Console.ReadLine();
+
+        }
+    }
+}
